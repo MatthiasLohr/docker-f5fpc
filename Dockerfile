2276de908661e1d3d7b98753bd7beab5d0d40542
Matthias Lohr <matthias@lohr.me>
2017-05-17 11:01:08 +0200

    first real working state

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 
 FROM ubuntu:latest
 
-RUN apt-get update && apt-get install -y vim wget && apt-get clean
+RUN apt-get update && apt-get install -y iptables vim wget && apt-get clean
 RUN mkdir -p /root/f5fpc && cd /root/f5fpc && wget --no-check-certificate https://72.3.241.247/public/download/linux_sslvpn.tgz && tar xfz linux_sslvpn.tgz && yes "yes" | ./Install.sh && rm -rf /root/f5fpc
 
 COPY ./connect.sh /root/connect.sh
--- connect.sh
+++ connect.sh
@@ -6,20 +6,24 @@ F5FPC=/usr/local/bin/f5fpc
 command="$F5FPC -s -x"
 
 if [ -n "$HOST" ] ; then
-	command="$command -t \"$HOST\""
+	command="$command -t $HOST"
 fi
 
 if [ -n "$USER" ] ; then
-	command="$command -u \"$USER\""
+	command="$command -u $USER"
 fi
 
 if [ -n "$PASSWORD" ] ; then
-	command="$command -p \"$PASSWORD\""
+	command="$command -p $PASSWORD"
 fi
 
-
+if [ -n "$HEXPASSWORD" ] ; then
+	command="$command -P $HEXPASSWORD"
+fi
 
 $command
 
-#sysctl -w net.ipv4.ip_forward=1
-#iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+sysctl -w net.ipv4.ip_forward=1
+iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
+
+sleep 1d
