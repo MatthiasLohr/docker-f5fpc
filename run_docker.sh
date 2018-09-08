5a2a36741e3195e535cd75b3289682799cb2d604
Alex Golts <alex@golts.net>
2018-09-08 08:23:59 +0300

    add display support on mac

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-docker run --net host -it --rm --privileged f5fpc_vpn /bin/bash
+open -a xquartz
+
+IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
+xhost + $IP
+
+docker run \
+	--net host \
+	-it --rm --privileged \
+	-e USER=$USER \
+	-e DISPLAY=$IP:0 \
+	-v /tmp/.X11-unix:/tmp/.X11-unix \
+	-e QT_X11_NO_MITSHM=1 \
+	-v /$HOME:/home/$USER \
+	f5fpc_vpn /bin/bash
 
