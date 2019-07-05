4a65115c09bfa4727dd56c3e8e08410f9ae15d29
Tim Steinbach <tim@nequissimus.com>
2019-07-04 20:30:03 -0400

    Fixes as per ShellCheck

--- f5fpc-vpn.sh
+++ f5fpc-vpn.sh
@@ -2,14 +2,12 @@
 
 DOCKER_IMAGE="matthiaslohr/f5fpc:latest@sha256:86418f9d612a8d3fc208c7296729b61c8a395de5aa5bb17a2848fdcc51f6c40b"
 CONTAINER_NAME="f5fpc-vpn"
-F5FPC_ARGS=""
 VPNHOST=""
 USERNAME=""
 keep_running=1
 
-for cmd in docker ip ; do
-  which "$cmd" > /dev/null 2> /dev/null
-  if [ "$?" != "0" ] ; then
+for cmd in docker ip; do
+  if ! command -v "$cmd" >/dev/null 2>&1; then
     echo "Unsatisfied dependencies: $cmd command not found!"
     exit 1
   fi
@@ -33,7 +31,7 @@ EOF
 observe_f5fpc() {
   last_result=-1
   while [ $keep_running ] ; do
-    output=`docker exec "$CONTAINER_NAME" /usr/local/bin/f5fpc -i`
+    output=$(docker exec "$CONTAINER_NAME" /usr/local/bin/f5fpc -i)
     result=$?
     case $result in
       0) # Everything seems to be ok
@@ -90,47 +88,45 @@ observe_f5fpc() {
 }
 
 start_client() {
-  docker run -d --rm --privileged \
+  if ! docker run -d --rm --privileged \
     --name "$CONTAINER_NAME" \
     --net host \
     -e VPNHOST="$VPNHOST" \
     -e USERNAME="$USERNAME" \
     "${DOCKER_IMAGE}" \
-    /opt/idle.sh > /dev/null
-  if [ "$?" != 0 ] ; then
-    echo "Error starting docker container."
-    exit 1
+    /opt/idle.sh > /dev/null; then
+      echo "Error starting docker container."
+      exit 1
   fi
   docker exec -it "$CONTAINER_NAME" /opt/connect.sh
   observe_f5fpc
 }
 
 start_gateway() {
-  docker run -d --rm --privileged \
+  if ! docker run -d --rm --privileged \
     --name "$CONTAINER_NAME" \
     --sysctl net.ipv4.ip_forward=1 \
     -e VPNHOST="$VPNHOST" \
     -e USERNAME="$USERNAME" \
     "${DOCKER_IMAGE}" \
-    /opt/idle.sh > /dev/null
-  if [ "$?" != 0 ] ; then
-    echo "Error starting docker container."
-    exit 1
+    /opt/idle.sh > /dev/null; then
+      echo "Error starting docker container."
+      exit 1
   fi
   docker exec -it "$CONTAINER_NAME" /opt/connect.sh
-  dockerip=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $CONTAINER_NAME`
-  for network in ${NETWORKS[@]} ; do
-    ip route add $network via $dockerip
+  dockerip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $CONTAINER_NAME)
+  for network in "${NETWORKS[@]}"; do
+    ip route add "$network" via "$dockerip"
   done
   observe_f5fpc
 }
 
 stop_vpn() {
   echo "Shutting down..."
-  dockerip=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $CONTAINER_NAME`
-        for network in ${NETWORKS[@]} ; do
-                ip route del $network via $dockerip
-        done
+  dockerip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $CONTAINER_NAME)
+  for network in "${NETWORKS[@]}"; do
+    ip route del "$network" via "$dockerip"
+  done
   docker exec "$CONTAINER_NAME" /usr/local/bin/f5fpc -o > /dev/null
   docker stop "$CONTAINER_NAME"
   exit
